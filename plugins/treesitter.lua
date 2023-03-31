ca268b020cdc4792ff7aaa0f9b2983719784b747
AlcorBBX <lev.matisov55@gmail.com>
2023-03-31 16:21:27 +0500

    add autoClosing tag plugin

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    -- ensure_installed = { "lua" },
+  require 'nvim-treesitter.configs'.setup {
+    autotag = {
+      enable = true
+    }
   },
+  -- opts = {
+  --   autoClosingTags = {
+  --     enable = true,
+  --   }
+  -- ensure_installed = { "lua" },
+  -- },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,56 +1,59 @@
-return { "HerringtonDarkholme/yats.vim", "morhetz/gruvbox", "folke/tokyonight.nvim", "mattn/emmet-vim",
-    -- "catppuccin/nvim", 
+return { "HerringtonDarkholme/yats.vim",
+    "morhetz/gruvbox",
+    "mattn/emmet-vim",
+    -- 'm4xshen/autoclose.nvim',
+    'windwp/nvim-ts-autotag',
     {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-        require("catppuccin").setup {
-            flavour = "mocha", -- latte, frappe, macchiato, mocha
-            term_colors = true,
-            transparent_background = true,
-            no_italic = false,
-            no_bold = false,
-            styles = {
-                comments = {},
-                conditionals = {},
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {}
-            },
-            color_overrides = {
-                mocha = {
-                    base = "#202023",
-                    mantle = "#202023",
-                    crust = "#202023"
-                }
-            },
-            highlight_overrides = {
-                mocha = function(C)
-                    return {
-                        TabLineSel = { bg = C.pink },
-                        CmpBorder = {
-                            fg = C.surface2
-                        },
-                        Pmenu = {
-                            bg = C.none
-                        },
-                        TelescopeBorder = {
-                            link = "FloatBorder"
-                        }
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup {
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                term_colors = true,
+                transparent_background = true,
+                no_italic = false,
+                no_bold = false,
+                styles = {
+                    comments = {},
+                    conditionals = {},
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {}
+                },
+                color_overrides = {
+                    mocha = {
+                        base = "#202023",
+                        mantle = "#202023",
+                        crust = "#202023"
                     }
-                end
+                },
+                highlight_overrides = {
+                    mocha = function(C)
+                        return {
+                            TabLineSel = { bg = C.pink },
+                            CmpBorder = {
+                                fg = C.surface2
+                            },
+                            Pmenu = {
+                                bg = C.none
+                            },
+                            TelescopeBorder = {
+                                link = "FloatBorder"
+                            }
+                        }
+                    end
+                }
             }
-        }
 
-        -- vim.cmd.colorscheme "catppuccin"
-    end
-} -- You can also add new plugins here as well:
+            -- vim.cmd.colorscheme "catppuccin"
+        end
+    } -- You can also add new plugins here as well:
     -- Add plugins, the lazy syntax
     -- "andweeb/presence.nvim",
     -- {
