00c112f36bc5014c7766027c2e68ba1f632b1a15
AlcorBBX <lev.matisov55@gmail.com>
2023-06-16 07:16:27 +0500

    feat(colorizer.lua): add colorizer plugin

---
+++ lua/user/colorizer.lua
@@ -0,0 +1,19 @@
+local M = {
+  "norcalli/nvim-colorizer.lua",
+  -- commit = "",
+  event = "InsertEnter",
+}
+
+function M.config()
+  require("colorizer").setup({ "css", "scss", "html", "javascript", "lua" }, {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    rgb_fn = true, -- CSS rgb() and rgba() functions
+    hsl_fn = true, -- CSS hsl() and hsla() functions
+    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  })
+end
+
+return M
