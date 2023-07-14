90b341e379724122917dd64ec66c12129367b39d
AlcorBBX <lev.matisov55@gmail.com>
2023-07-14 20:03:11 +0500

    feat(null-ls): add format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -8,8 +8,9 @@ local b = null_ls.builtins
 
 local sources = {
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with {
+  b.formatting.deno_fmt.with { extra_args = { "--use-tabs", "--single-quote" } }, -- choosed deno for ts/js files cuz its very fast!
+
+  b.formatting.prettierd.with {
     filetypes = {
       "html",
       "markdown",
@@ -24,6 +25,21 @@ local sources = {
     },
   }, -- so prettier works only on these filetypes
 
+  -- b.formatting.prettier.with {
+  --   filetypes = {
+  --     "html",
+  --     "markdown",
+  --     "css",
+  --     "scss",
+  --     -- "typescript",
+  --     -- "javascript",
+  --     -- "typescriptreact",
+  --     -- "javascriptreact",
+  --     -- "tsx",
+  --     -- "jsx",
+  --   },
+  -- }, -- so prettier works only on these filetypes
+
   -- Lua
   b.formatting.stylua,
 
@@ -31,7 +47,43 @@ local sources = {
   b.formatting.clang_format,
 }
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  }
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
+
 null_ls.setup {
-  debug = true,
+  debug = false,
   sources = sources,
+  debounce = 300,
+  -- format on save
+  on_attach = function()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
 }
