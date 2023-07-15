7c5dbfcd9260606758b7e6815071f61fb06d12dc
AlcorBBX <lev.matisov55@gmail.com>
2023-07-15 14:24:48 +0500

    feat(plugins.lua): add support jsx/tsx comments

--- plugins.lua
+++ plugins.lua
@@ -66,6 +66,15 @@ local plugins = {
     lazy = false,
   },
   {
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
+  {
     "windwp/nvim-ts-autotag",
     config = function()
       require("nvim-ts-autotag").setup()
