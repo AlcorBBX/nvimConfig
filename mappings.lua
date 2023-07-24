04f98709ed914c405e328ca39a12d7c499121a94
AlcorBBX <lev.matisov55@gmail.com>
2023-07-24 18:24:12 +0500

    feat(mapping.lua): update mappings

--- mappings.lua
+++ mappings.lua
@@ -83,12 +83,12 @@ M.general = {
       "Which-key query lookup",
     },
     ["<leader>h"] = { ":noh <CR>", "Clear highlights" },
-    ["gl"] = {
-      function()
-        vim.diagnostic.open_float { border = "rounded" }
-      end,
-      "Floating diagnostic",
-    },
+    -- ["gl"] = {
+    --   function()
+    --     vim.diagnostic.open_float { border = "rounded" }
+    --   end,
+    --   "Floating diagnostic",
+    -- },
     ["<leader>tp"] = {
       ":Telescope projects<CR>",
       -- ":lua require('telescope').extensions.projects.projects()<CR>",
@@ -100,24 +100,25 @@ M.general = {
 
     -- lspsaga
     ["<leader>sk"] = { ":Lspsaga hover_doc <CR>", "LspSaga hover doc", opts = { silent = true } },
-    ["<leader>spd"] = { ":Lspsaga peek_definition <CR>", "LspSaga peek definition", opts = { silent = true } },
-    ["<leader>spD"] = { ":Lspsaga peek_type_definition <CR>", "LspSaga peek type definition", opts = { silent = true } },
+    ["<leader>sp"] = { ":Lspsaga peek_definition <CR>", "LspSaga peek definition", opts = { silent = true } },
+    ["<leader>sP"] = { ":Lspsaga peek_type_definition <CR>", "LspSaga peek type definition", opts = { silent = true } },
 
     ["<leader>sD"] = {
       ":Lspsaga show_workplace_diagnostics <CR>",
       "LspSaga show workspace diagnostic",
       opts = { silent = true },
     },
+    ["gl"] = { ":Lspsaga diagnostic_jump_next <CR>", "LspSaga diagnostic", opts = { silent = true } },
     ["<leader>sdn"] = { ":Lspsaga diagnostic_jump_next <CR>", "LspSaga diagnostic next", opts = { silent = true } },
     ["<leader>sdp"] = { ":Lspsaga diagnostic_jump_prev <CR>", "LspSaga diagnostic prev ", opts = { silent = true } },
 
     ["<leader>sgD"] = { ":Lspsaga goto_type_definition<CR>", "LspSaga goto_type_definition", opts = { silent = true } },
-    ["<leader>spr"] = { ":Lspsaga project_replace <CR>", "LspSaga project replace", opts = { silent = true } },
+    ["<leader>sR"] = { ":Lspsaga project_replace <CR>", "LspSaga project replace", opts = { silent = true } },
     ["<leader>sr"] = { ":Lspsaga rename <CR>", "LspSaga rename", opts = { silent = true } },
     ["<leader>soc"] = { ":Lspsaga outgoing_calls <CR>", "LspSaga outgoing calls", opts = { silent = true } },
     ["<leader>sic"] = { ":Lspsaga incomming_calls <CR>", "LspSaga incomming calls", opts = { silent = true } },
     ["<leader>sa"] = { ":Lspsaga code_action <CR>", "LspSaga code actions", opts = { silent = true } },
-    ["<leader>sgl"] = { ":Lspsaga show_line_diagnostics <CR>", "LspSaga line diagnostic", opts = { silent = true } },
+    -- ["gl"] = { ":Lspsaga show_line_diagnostics <CR>", "LspSaga line diagnostic", opts = { silent = true } },
     -- ["<leader>s"] = { ":Lspsaga show_buf_diagnostics <CR>", "LspSaga ", opts = { silent = true } },
     -- ["<leader>s"] = { ":Lspsaga show_cursor_diagnostics <CR>", "LspSaga ", opts = { silent = true } },
     ["<leader>sgd"] = { ":Lspsaga goto_definition <CR>", "LspSaga go to definition", opts = { silent = true } },
